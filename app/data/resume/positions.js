9e153b3dd172d7e39f6a4bd500f7e079e5a2b96b
jyj <yjjiangphysics@gmail.com>
2020-01-16 21:45:27 -0600

    update resume

--- app/data/resume/positions.js
+++ app/data/resume/positions.js
@@ -15,7 +15,7 @@ const positions = [
     link: '/',
     daterange: 'August 2018 - Present',
     points: [
-      'Used supervised and unsupervised machine learning algorithms to analyze the Yelp challenge dataset and built a recommendatin system.',
+      'Used supervised and unsupervised machine learning algorithms to analyze the Yelp challenge dataset and built a recommendation system.',
       'Learned the basics of causal inference and finished a challenge for simulated random dataset on directed acyclic graphs(DAG)',
       'Learned the basics about the reinforcement learning from the book by Richard S. Sutton and Andrew G. Barto, and used Deep Q-learning (DQL) and deep deterministic policy gradient (DDPG) to solve the quantum protocal(i.e., time dependence of the system) to drive a simple quantum system into a particular state.',
       'Built multiple dashboards for data visualization  using Flask, D3, Plotly, and React, ',
@@ -25,11 +25,37 @@ const positions = [
       'Obtained "deep learning specialization" certificate from Coursera Specialization Certificate',
       'Obtained "Data Visualization and Analytics" certificate from Trilogy Education at University of Minnesota(twin cities)'
     ],
-  }, {
+  },
+  {
+    company: 'Ningbo Institute of Industrial Technology, Chinese Academy of Sciences',
+    position: 'Visiting Professor',
+    link: 'http://english.nimte.cas.cn/',
+    daterange: 'January 2018 - May 2015',
+    points: [
+      'Successfully commanded some group theoretical methods and shared the skill within the work group.',
+      'Constructed an effective model for a system with topological nodal ring using the new approach.',
+      'Analyzed the DFT dataset obtained by the computation team and uncover the underlying physics story.',
+      'Follow up exciting progresses on the academic frontier and propose new research projects.',
+      'Proposed a project on twisted bilayer graphene and successfully calculated the Landau-levels (Hofstadter-Butterfly) for twisted bilayer graphene, the star material of 2018.',
+    ],
+  },
+
+  {
+    company: 'National Sun Yat-Sen University',
+    position: 'Visiting Scholar',
+    link: 'https://twin-cities.umn.edu/',
+    daterange: 'September 2017 - December 2017',
+    points: [
+      'Explore the application potential of topological materials in the direction of spintronics.',
+      'Proved some novel symmetry relation of spin transport within a theoretical model of a Weyl semimetal.',
+    ],
+  },
+
+  {
     company: 'University of Minnesota',
     position: 'Postdoc Associate',
     link: 'https://twin-cities.umn.edu/',
-    daterange: 'Jan 2015 - Jan 2017',
+    daterange: 'January 2015 - January 2017',
     points: [
       'Investigated the transport property of novel two dimensional crystals, especially thin film of black phosphorous(BP).  ',
       'Sucessfully published two journal papers on BP and received more than 50 citations from international colleagues.',
@@ -54,6 +80,16 @@ const positions = [
       'Taught the courses for physics majors such as electrodynamics, quantum mechanics,linear algebra, solid state theory, computational physics, and quantum field theory.',
       'Published more than 30 scientific papers (see here for my publication list) with more than 1000 citations. Won a research leadership award.  ',
       'Mentored 20 undergraduates and 10 postgraduates.',
+    ],
+  },
+  {
+    company: 'Tsinghua University',
+    position: 'Postdoc Researcher',
+    link: 'https://www.tsinghua.edu.cn/publish/thu2018en/index.html',
+    daterange: 'September 2002 - July 2004',
+    points: [
+      'Research on strongly correlated system on triangular lattice, wrote three scientific papers.',
+      'Syatematically studied the phase diagram for t-J model on triangular superconductor',
     ],
   },
   // {
--- app/views/Index.js
+++ app/views/Index.js
@@ -16,7 +16,7 @@ const Index = () => (
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
+        {/* view <Link to="/stats">site statistics</Link>, {' '} */}
         or <Link to="/contact">contact</Link> me.
       </p>
       {/* <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p> */}
